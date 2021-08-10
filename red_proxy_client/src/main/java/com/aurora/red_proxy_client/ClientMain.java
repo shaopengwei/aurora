3e8d0a6d37c7e8fd1d93302d25400ab20278dcb3
shaopengwei <shaopengwei@sensorsdata.cn>
2021-08-11 00:28:55 +0800

    2021-08-11 0:28

---
+++ red_proxy_client/src/main/java/com/aurora/red_proxy_client/ClientMain.java
@@ -0,0 +1,21 @@
+package com.aurora.red_proxy_client;
+
+import com.aurora.red_proxy_client.client.Client;
+
+/**
+ * @author shaopengwei@hotmail.com
+ * @Description
+ * @create 2021-08-10 23:19
+ */
+public class ClientMain {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: " + Client.class.getSimpleName() + " <host> <port>");
+            return;
+        }
+        final String host = args[0];
+        final int port = Integer.parseInt(args[1]);
+        new Client(host, port).run();
+    }
+}
---
+++ red_proxy_client/src/main/java/com/aurora/red_proxy_client/handler/EchoClientHandler.java
@@ -0,0 +1,49 @@
+package com.aurora.red_proxy_client.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author shaopengwei@hotmail.com
+ * @Description
+ * @create 2021-08-10 23:42
+ */
+@ChannelHandler.Sharable
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    /**
+     * 建立连接后该 channelActive() 方法被调用一次
+     * 字节序列被发送到服务器
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+
+    /**
+     * 方法会在接收到数据时被调用
+     *
+     * @param ctx
+     * @param msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in  = (ByteBuf) msg;
+        System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
