ed29d4c145e17edb1039f54e6c773e7684f7631a
shaopengwei <shaopengwei@shaopenweideMBP.lan>
2021-08-10 12:20:43 +0800

    red_proxy

---
+++ Red_proxy/src/main/java/com/aurora/red_proxy/ServerMain.java
@@ -0,0 +1,24 @@
+package com.aurora.red_proxy;
+
+import com.aurora.red_proxy.server.DiscardServer;
+
+/**
+ * TODO
+ *
+ * @author shaopengwei
+ * @version 1.0.0
+ * @since 2021/08/10 11:43
+ */
+public class ServerMain {
+
+  public static void main(String[] args) throws Exception {
+    int port;
+    if (args.length > 0) {
+      port = Integer.parseInt(args[0]);
+    } else {
+      port = 8080;
+    }
+    new DiscardServer(port).run();
+    System.out.println("server:run()");
+  }
+}
